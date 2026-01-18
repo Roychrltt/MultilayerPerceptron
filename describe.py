ca68cfa2c05ff7a684dd3e0bdb1fa02a7dde917c
Charlotte <roychrltt@gmail.com>
2026-01-18 18:30:01 +0100

    describe done

--- describe.py
+++ describe.py
@@ -2,94 +2,69 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
-from nn import *
 
 
-def data_split(df):
-    X = df.iloc[:, 1:].values
-    y = df.iloc[:, 0].values
-
-    idx = np.random.permutation(len(X))
-    split = int(0.8 * len(X))
-
-    train_idx = idx[:split]
-    test_idx = idx[split:]
-
-    X_train, X_test = X[train_idx], X[test_idx]
-    y_train, y_test = y[train_idx], y[test_idx]
-    return X_train, X_test, y_train, y_test
-
-
-def data_describe(filename):
-    cols = [""] + ["diagnosis"] + [f"No.{i}" for i in range (1, 31)]
-
-    df = pd.read_csv(filename, header=None, names=cols)
+def read_data(filename):
+    """Read data in csv with pandas and return the df"""
+    df = pd.read_csv(filename, header=None)
     df = df.drop(df.columns[0], axis=1)
+    cols = ["label"] + [f"No.{i}" for i in range (1, 31)]
+    df.columns = cols
+    df.iloc[:, 0] = df.iloc[:, 0].map({"M":1, "B":0})
+    return df
 
+
+def data_describe(df):
+    """Describe data with pandas.describe() and visualize data with scatter plots"""
     pd.set_option("display.max_columns", None)
     print(df.describe())
     pd.reset_option("display.max_columns")
-
     print(df.iloc[:, 0].value_counts())
-    df.iloc[:, 0] = df.iloc[:, 0].map({"M":1, "B":0})
-    df.iloc[:, 1:].hist(figsize=(15,10), bins=100)
-    plt.tight_layout()
-    #plt.show()
-    df.iloc[:, 1:].plot(kind="box", figsize=(15,5))
-    plt.tight_layout()
-    #plt.show()
 
+    mask_M = df["label"] == 1
+    mask_B = df["label"] == 0
+    fig, axes = plt.subplots(5, 6, figsize=(18, 10))
+    axes = axes.flatten()
+    features = df.columns[1:]
+    for i, col in enumerate(features):
+        ax = axes[i]
+
+        ax.scatter(
+            df.index[mask_M],
+            df.loc[mask_M, col],
+            label="M",
+            alpha=0.6,
+            s=8
+        )
+
+        ax.scatter(
+            df.index[mask_B],
+            df.loc[mask_B, col],
+            label="B",
+            alpha=0.6,
+            s=8
+        )
+
+        ax.set_title(col)
+    
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="upper right")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def clean_data(df):
+    useful = ["label"] + [f"No.{i}" for i in [1,3,4,6,7,8,11,13,14,21,22,23,24,26,27,28]]
+    df = df[useful]
     return df
-
-def binary_cross_entropy(logit, y):
-    x = logit
-    max_part = x.relu()
-    log_part = ((-x.abs()).exp() + Value(1)).log()
-    return max_part - x*y + log_part
-
 
 
 def main():
     filename = "data.csv"
-    df = data_describe(filename)
-    X_train, X_test, y_train, y_test = data_split(df)
-    min_val = X_train.min(axis=0)
-    max_val = X_train.max(axis=0)
-
-    X_train_norm = (X_train - min_val) / (max_val - min_val)
-    X_test_norm = (X_test - min_val) / (max_val - min_val)
-    mlp = MLP(30, [16, 8, 1])
-
-    lr = 0.001
-    epochs = 100
-    for epoch in range(epochs):
-        total_loss = 0.0
-
-        for x, y in zip(X_train_norm, y_train):
-            x = x.tolist()
-
-            mlp.zero_grad()
-            logit = mlp(x)
-            loss = binary_cross_entropy(logit, y)
-            total_loss += loss.data
-
-            loss.backward()
-
-            for p in mlp.parameters():
-                p.data -= lr * p.grad
-
-        print(f"epoch {epoch}, loss {total_loss / len(X_train)}")
-
-    correct = 0
-
-    for x, y in zip(X_test_norm, y_test):
-        p = mlp(x.tolist()).relu().data
-        pred = 1 if p > 0.5 else 0
-        correct += (pred == y)
-
-    accuracy = correct / len(X_test)
-    print("test accuracy:", accuracy)
-
+    df = read_data(filename)
+    data_describe(df)
+ 
 
 if __name__ == "__main__":
     main()

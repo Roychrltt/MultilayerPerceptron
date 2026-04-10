858932aabd35f4b927166749e67636631d4e46fe
Charlotte <roychrltt@gmail.com>
2026-04-10 17:12:00 +0200

    FEAT: more robust describe.py.

--- describe.py
+++ describe.py
@@ -6,32 +6,46 @@ import sys
 from scipy import stats
 
 
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
 def read_clean_data(filename, handle_nan='mean'):
     """ Use pandas to read from data.csv, check for consistent types per column and handle NaNs.
 
         handle_nan: 'mean' to fill with average, 'drop' to remove rows.
 
         The first column (patient ID) is dropped.
-        The lable column is converted from str to int to facilitate later training.
+        The label column is converted from str to int to facilitate later training.
 
     """
     df = pd.read_csv(filename, header=None)
-    #print(df.describe().T)
+    print('='*45+' Describe Begin '+'='*45)
+    print(df.describe().T)
+    print('='*45+' Describe End '+'='*45)
 
     df = df.drop(df.columns[0], axis=1)
-    df.columns = range(df.shape[1])
+    cols = ["label"] + [f"No.{i}" for i in range(1, 31)]
+    df.columns = cols
     n_col = df.shape[1]
     valid_labels = ['M', 'B']
     initial_count = len(df)
-    df = df[df[0].isin(valid_labels)].copy()
+    df = df[df['label'].isin(valid_labels)].copy()
 
     if len(df) < initial_count:
         print(f"🗑️ Dropped {initial_count - len(df)} samples with invalid labels.")
 
-    mapping = {'M': 1, 'B': 0}
-    df[df.columns[0]] = df[df.columns[0]].map(mapping)
+    df["label"] = df["label"].map({'M': 1, 'B': 0})
 
-    for col in range(1, n_col):
+    for col in df.columns[1:]:
         series = df[col]
         numeric_series = pd.to_numeric(series, errors='coerce')
         
@@ -53,6 +67,45 @@ def read_clean_data(filename, handle_nan='mean'):
     return df
 
 
+def data_visualise(df):
+    """ Visualise data distribution for different features.
+
+    """
+    mask_M = df["label"] == 1
+    mask_B = df["label"] == 0
+
+    fig, axes = plt.subplots(5, 6, figsize=(18, 10))
+    axes = axes.flatten()
+    features = df.columns[1:]
+    for i, col in enumerate(features):
+        ax = axes[i]
+
+        ax.scatter(
+            df.index[mask_M],
+            df.loc[mask_M, col],
+            label="M",
+            alpha=0.6,
+            s=8
+        )
+
+        ax.scatter(
+            df.index[mask_B],
+            df.loc[mask_B, col],
+            label="B",
+            alpha=0.6,
+            s=8
+        )
+
+        ax.set_title(col)
+            
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="upper right")
+
+    plt.tight_layout()
+    plt.show()
+    plt.close(fig)
+
+
 def drop_noisy_features(df, threshold=1e-21):
     """ Use Mann-Whitney U test to drop noisy features where two class do not differ significantly.
         If p_value is higher than threshold, it means the feature is noisy.
@@ -61,20 +114,24 @@ def drop_noisy_features(df, threshold=1e-21):
     class1 = df[df.iloc[:, 0] == 0]
     class2 = df[df.iloc[:, 0] == 1]
 
-    good_features = [df.columns[0]] # To keep the lable column
+    good_features = [df.columns[0]] # To keep the label column
     n_features = df.shape[1]
     for i in range(1, n_features):
         col = df.columns[i]
         stat, p_value = stats.mannwhitneyu(class1[col], class2[col])
         if p_value < threshold:
             good_features.append(col)
+        else:
+            print(f'{Color.YELLOW}Feature {i} being dropped.{Color.END}')
     return df[good_features]
 
 
-def data_split_and_save(df):
+def data_split_and_save(df, output_dir='data'):
     """ Split the data into train, val, and test set, and save them into corresponding files.
 
     """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     idx = np.random.permutation(len(df))
     split1 = int(0.7 * len(df))
     split2 = int(0.85 * len(df))
@@ -90,6 +147,7 @@ def data_split_and_save(df):
     df_train.to_csv("train.csv", index=False, header=False)
     df_val.to_csv("val.csv", index=False, header=False)
     df_test.to_csv("test.csv", index=False, header=False)
+    print(f'{Color.GREEN}Data preprocessing finished. Results saved to data/train.csv, data/val.csv, and data/test.csv.{Color.END}')
     
 
 def main():
@@ -100,7 +158,16 @@ def main():
         print("Usage: python3 describe.py <path_to_csv>")
         return
     filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print(f"{Color.RED}[ERROR]{Color.END} File '{filename}' not found.")
+        return
+
     df = read_clean_data(filename)
+    if df.empty:
+        print(f"{Color.RED}[ERROR]{Color.END} No valid data remaining after cleaning.")
+        return
+
+    data_visualise(df)
     df = drop_noisy_features(df)
     data_split_and_save(df)
  

FIX: fixed double-scaling issue, fine tuned batch size and lr for pytorch and tensorflow models. Test accuracy reached 98+%

srcs/pytorchpredict.py
@@ -2,10 +2,13 @@ import torch
 import pandas as pd
 from torch.utils.data import DataLoader, TensorDataset
 from pytorchtrain import MLP, load_and_tensorize, Color
+import joblib
 
-def evaluate_on_test(model_path="model/best_model.pth", test_path="data/test.csv", hidden_config=[64, 64, 32]):
+def evaluate_on_test(model_path="model/best_model.pth", test_path="data/test.csv", scaler_path="data/scaler.joblib", hidden_config=[64, 64, 32]):
     """ Loads model and tests the model's prediction accuracy. """
     X_test, y_test = load_and_tensorize(test_path)
+    scaler = joblib.load(scaler_path)
+    X_test = torch.tensor(scaler.transform(X_test), dtype=torch.float32)
     test_loader = DataLoader(TensorDataset(X_test, y_test), batch_size=32)
 
     model = MLP(input_size=X_test.shape[1], hidden_layers=hidden_config)
@@ -27,8 +30,7 @@ def evaluate_on_test(model_path="model/best_model.pth", test_path="data/test.csv
             all_preds.extend(preds.numpy())
 
     accuracy = total_correct / len(y_test)
-    print(f"✅ Evaluation Complete!")
-    print(f"📊 Test Accuracy: {accuracy * 100:.2f}%")
+    print(f"Test Accuracy: {accuracy * 100:.2f}%")
     
     return all_preds
 

srcs/tensorflowpredict.py
@@ -1,21 +1,22 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import joblib
 import os
 
-def predict_with_tensorflow(model_path='model/best_tf_model.h5', test_path='data/test.csv'):
+def predict_with_tensorflow(model_path='model/best_tf_model.h5', test_path='data/test.csv', scaler_path='data/scaler.joblib'):
     if not os.path.exists(model_path):
         print(f"Error: Model file {model_path} not found.")
         return
     
     model = tf.keras.models.load_model(model_path) 
     
+    scaler = joblib.load(scaler_path)
     test_df = pd.read_csv(test_path, header=None)
     y_test = test_df.iloc[:, 0].values
     X_test = test_df.iloc[:, 1:].values
 
-    X_test_scaled = X_test
-
+    X_test_scaled = scaler.transform(X_test)
     probabilities = model.predict(X_test_scaled, verbose=0).flatten()
 
     predictions = (probabilities > 0.5).astype(int)
@@ -30,7 +31,7 @@ def predict_with_tensorflow(model_path='model/best_tf_model.h5', test_path='data
         'Confidence': probabilities
     })
     pd.set_option('display.max_rows', None)
-    print(results)
+    #print(results)
 
     return predictions
 